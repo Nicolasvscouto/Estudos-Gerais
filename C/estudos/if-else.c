ca204f0bdb95304b5a7bab38270ef61e2b43c79b
Nicolasvscouto <168155572+Nicolasvscouto@users.noreply.github.com>
2025-03-06 10:24:19 -0300

    Create if-else.c

---
+++ C/estudos/if-else.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+    int numero1 = 10, numero2 = 20;
+
+    if(numero1 > numero2){
+        printf("Oie");
+    }else{
+        printf("Salve");
+    }
+
+    return 0;
+}
